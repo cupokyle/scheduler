68ba0038cc56894f245e43e16b37587a0b02401e
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-09-03 20:54:45 -0400

    Fix bug regarding schedule array in App.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -43,7 +43,6 @@ const schedule = appointments.map((appointment) => {
   axios.get("http://localhost:8001/api/interviewers")
 ]).then((all) => {
   setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-  console.log(all[2].data)
 });
 
   }, [])
@@ -72,7 +71,7 @@ const schedule = appointments.map((appointment) => {
       </section>
       <section className="schedule">
         
-        {appointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>)}
+        {schedule}
         <Appointment key="last" time="5pm"/>
         
       </section>
