88850f815a4b14a78a8c496a837c33d277c2c383
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-09-08 14:26:28 -0400

    Test cleanup & installation of cypress

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
+export default function Form(props) {
+  const { interviewers } = props;
 
-export default function Form (props) {
-
-  const {interviewers, onSave} = props;
+  const [error, setError] = useState("");
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
@@ -13,37 +13,56 @@ export default function Form (props) {
   const reset = () => {
     setName("");
     setInterviewer(null);
-  }
+  };
 
   const cancel = () => {
     reset();
     props.onCancel();
+  };
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
   }
 
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        value={name}
-        onChange={event => setName(event.target.value)}
-        /*
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
+            /*
           This must be a controlled component
         */
-      />
-    </form>
-    <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={() => onSave(name, interviewer)}>Save</Button>
-    </section>
-  </section>
-</main>
+          />
+          <section className="appointment__validation">{error}</section>
+        </form>
+        <InterviewerList
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
   );
 }
